eb2f5cc02b95da191ca6f0ac2bf8ab7def75044c
Ryan J. Shaw <rjs@lusr.org>
2023-02-16 17:15:29 +0200

    Set 1 Challenge 2 - Fixed XOR

--- Set1Challenge1/Program.cs
+++ Set1Challenge1/Program.cs
@@ -4,6 +4,6 @@ var hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706
 var expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
 var bytes = Convert.FromHexString(hexString);
-var base64 = Convert.ToBase64String(bytes);
+var actual = Convert.ToBase64String(bytes);
 
-Challenge.Outcome("Set 1 Challenge 1", base64 == expected);
+Challenge.Outcome("Set 1 Challenge 1", actual == expected);
---
+++ Set1Challenge2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Shared;
+
+var expected = "746865206b696420646f6e277420706c6179".ToUpper();
+
+var value = Convert.FromHexString("1c0111001f010100061a024b53535009181c");
+var key = Convert.FromHexString("686974207468652062756c6c277320657965");
+for (var i = 0; i < key.Length; i++)
+    value[i] ^= key[i];
+
+var actual = Convert.ToHexString(value);
+Challenge.Outcome("Set 1 Challenge 2", actual == expected);
