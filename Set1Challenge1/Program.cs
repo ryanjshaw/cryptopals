4348df87d1d8756914d897e3ae202c0d57437af2
Ryan J. Shaw <rjs@lusr.org>
2023-02-16 17:06:48 +0200

    Add project files.

---
+++ Set1Challenge1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Shared;
+
+var hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+var expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+var bytes = Convert.FromHexString(hexString);
+var base64 = Convert.ToBase64String(bytes);
+
+Challenge.Outcome("Set 1 Challenge 1", base64 == expected);
---
+++ Shared/Challenge.cs
@@ -0,0 +1,23 @@
+﻿namespace Shared
+{
+    public static class Challenge
+    {
+        public static void Outcome(string name, bool ok)
+        {
+            if (ok)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(name + ": OK");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(name + ": FAIL");
+                Console.ResetColor();
+            }
+        }
+    }
+}
